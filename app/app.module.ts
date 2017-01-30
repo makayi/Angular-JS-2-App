b13c072a66db7c5f630e50ba89cf2c3a5a30e051
mbuyu <mbmakayi@gmail.com>
2017-01-30 17:20:18 +0200

    Added the routing module and the home component

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,11 +6,12 @@ import { UsersComponent }  from './components/users/users.component';
 import {NavBarComponent  } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 import {routing} from './app.router';
 
 @NgModule({
   imports:      [ BrowserModule,routing],
-  declarations: [ AppComponent,UsersComponent, NavBarComponent,ContactComponent,AboutComponent],
+  declarations: [ AppComponent,UsersComponent, NavBarComponent,ContactComponent,AboutComponent,HomeComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/components/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId:module.id,
+  selector: 'home',
+  templateUrl: 'home.component.hmtl',
+})
+export class HomeComponent  {  }
